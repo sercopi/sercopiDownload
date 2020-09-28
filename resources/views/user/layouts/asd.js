4f8e877bec0eef6f86f6b40ad04a46a73f7db186
Akuma <sergiiosercopi@gmail.com>
2020-09-28 22:19:23 +0200

    deleted useles file

--- resources/views/user/layouts/asd.js
+++
@@ -1,9 +0,0 @@
-ClassicEditor.create(document.querySelector("#editor"))
-    .then(editor => {
-        editor.model.document.on("change:data", editor => {
-            console.log(editor.getData());
-        });
-    })
-    .catch(error => {
-        console.error(error);
-    });
